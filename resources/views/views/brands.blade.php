beef55da5fcb41d9133ca28bb3a3291d4956f9f5
DoAnWebBanHang <trinhthinh279@gmail.com>
2016-12-26 15:05:41 +0700

    Đổ dữ liệu

--- resources/views/views/brands.blade.php
+++ resources/views/views/brands.blade.php
@@ -7,13 +7,21 @@
       	 <h1 class="page-heading product-listing">
 			Thương Hiệu
          </h1>
+         <?php
+   		$connect = connect();
+   		$result = mysqli_query($connect, "SELECT * from thuonghieu");
+   		while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){ 
+			$tenthuonghieu = trim($row['tenthuonghieu']);		
+			$gioithieu	=trim($row['gioithieu']);	
+			$hinhanh=trim($row['hinhanhthuonghieu']);		
+		?>
           <div class="brand_box">
 	         <div class="left-side col-xs-12 col-sm-3">
-				 <img src="images/2nd-day.jpg" alt=""/>
+				 <img src="<?php echo $hinhanh ?>" alt=""/>
 			 </div>
 		     <div class="middle-side col-xs-12 col-sm-5">
-		     	<h4><a href="Rolex">Rolex</a></h4>
-		     	<p>Một vương miện cho mỗi thành tích</p>
+		     	<h4><a href="Rolex"><?php echo $tenthuonghieu ?></a></h4>
+		     	<p><?php echo $gioithieu ?></p>
 			 </div>
 			 <div class="right-side col-xs-12 col-sm-4">
 			 	<p><a href="#">1 sản phẩm</a></p>
@@ -21,93 +29,21 @@
 			 </div>
 			 <div class="clearfix"> </div>
 		  </div>
-		  <div class="brand_box">
-	         <div class="left-side col-xs-12 col-sm-3">
-				 <img src="images/mih-jeans.jpg" alt=""/>
-			 </div>
-		     <div class="middle-side col-xs-12 col-sm-5">
-		     	<h4><a href="Zenith">Zenith</a></h4>
-		     	<p>Theo dõi ngôi sao của chính bạn</p>
-			 </div>
-			 <div class="right-side col-xs-12 col-sm-4">
-			 	<p><a href="#">1 sản phẩm</a></p>
-			    <a href="#" class="btn btn1 btn-primary btn-normal btn-inline " target="_self">View Products</a>     
-			 </div>
-			 <div class="clearfix"> </div>
-		  </div>
-		  <div class="brand_box">
-	         <div class="left-side col-xs-12 col-sm-3">
-				 <img src="images/g-star-raw.jpg" alt=""/>
-			 </div>
-		     <div class="middle-side col-xs-12 col-sm-5">
-		     	<h4><a href="Sarcar">Sarcar</a></h4>
-		     	<p>Chỉ cần lên dây cót là chạy mãi</p>
-			 </div>
-			 <div class="right-side col-xs-12 col-sm-4">
-			 	<p><a href="#">1 sản phẩm</a></p>
-			    <a href="#" class="btn btn1 btn-primary btn-normal btn-inline " target="_self">View Products</a>     
-			 </div>
-			 <div class="clearfix"> </div>
-		  </div>
-		  <div class="brand_box">
-	         <div class="left-side col-xs-12 col-sm-3">
-				 <img src="images/weekday1.jpg" alt=""/>
-			 </div>
-		     <div class="middle-side col-xs-12 col-sm-5">
-		     	<h4><a href="Oris">Oris</a></h4>
-		     	<p>Đồng hồ thật cho người thật</p>
-			 </div>
-			 <div class="right-side col-xs-12 col-sm-4">
-			 	<p><a href="#">1 sản phẩm</a></p>
-			    <a href="#" class="btn btn1 btn-primary btn-normal btn-inline " target="_self">View Products</a>     
-			 </div>
-			 <div class="clearfix"> </div>
-		  </div>
-		  <div class="brand_box">
-	         <div class="left-side col-xs-12 col-sm-3">
-				 <img src="images/longines.jpg" alt=""/>
-			 </div>
-		     <div class="middle-side col-xs-12 col-sm-5">
-		     	<h4><a href="Longines">Longines</a></h4>
-		     	<p>Sự thanh lịch là một thái độ</p>
-			 </div>
-			 <div class="right-side col-xs-12 col-sm-4">
-			 	<p><a href="#">1 sản phẩm</a></p>
-			    <a href="#" class="btn btn1 btn-primary btn-normal btn-inline " target="_self">View Products</a>     
-			 </div>
-			 <div class="clearfix"> </div>
-		  </div>
-		  <div class="brand_box">
-	         <div class="left-side col-xs-12 col-sm-3">
-				 <img src="images/casio.jpg" alt=""/>
-			 </div>
-		     <div class="middle-side col-xs-12 col-sm-5">
-		     	<h4><a href="#">Casio</a></h4>
-		     	<p>Công nghệ cho cuộc sống</p>
-			 </div>
-			 <div class="right-side col-xs-12 col-sm-4">
-			 	<p><a href="Casio">1 sản phẩm</a></p>
-			    <a href="#" class="btn btn1 btn-primary btn-normal btn-inline " target="_self">View Products</a>     
-			 </div>
-			 <div class="clearfix"> </div>
-		  </div>
-		  <div class="brand_box">
-	         <div class="left-side col-xs-12 col-sm-3">
-				 <img src="images/orient.jpg" alt=""/>
-			 </div>
-		     <div class="middle-side col-xs-12 col-sm-5">
-		     	<h4><a href="Orient">Orient</a></h4>
-		     	<p>Sống trọn đam mê</p>
-			 </div>
-			 <div class="right-side col-xs-12 col-sm-4">
-			 	<p><a href="#">1 sản phẩm</a></p>
-			    <a href="#" class="btn btn1 btn-primary btn-normal btn-inline " target="_self">View Products</a>     
-			 </div>
-			 <div class="clearfix"> </div>
-		  </div>
+		  <?php 
+		}
+   		mysqli_close($connect);
+		?>	
 		</div>
 		
      <div class="clearfix"> </div>
 	</div>
    </div>
-    @stop
+<?php
+	function connect(){	
+		$connect = mysqli_connect("localhost", "root", "") or die(mysql_error());
+		mysqli_set_charset($connect, 'utf8');
+		mysqli_select_db($connect, "watches") or die(mysql_error());
+		return $connect;
+	}
+?>
+@stop
--- resources/views/views/men.blade.php
+++ resources/views/views/men.blade.php
@@ -3,131 +3,52 @@
 @section('noidung')
 <div class="pro">   
    <ul class="products">
-   
-   <li class="simpleCart_shelfItem">
-							<a class="cbp-vm-image" href="single">
-							 <div class="view view-first">
-					   		  <div class="inner_content clearfix">
-								<div class="product_image">
-									<div class="mask1"><img src="images/m4.jpg" alt="image" class="img-responsive zoom-img"></div>
-									<div class="mask">
-			                       		<div class="info">Quick View</div>
-					                 </div>
-					                 <div class="product_container">
-									   <h4>Lorem 2015</h4>
-									   <p>Dresses</p>
-									   <div class="price mount item_price">$99.00</div>
-									   <a class="button item_add cbp-vm-icon cbp-vm-add" href="#">Add to cart</a>
-									 </div>		
-								  </div>
-			                     </div>
-		                      </div>
-		                    </a>
-						</li>
-						<li class="simpleCart_shelfItem">
-							<a class="cbp-vm-image" href="single">
-							  <div class="view view-first">
-					   		  <div class="inner_content clearfix">
-								<div class="product_image">
-									<div class="mask1"><img src="images/m2.jpg" alt="image" class="img-responsive zoom-img"></div>
-									 <div class="mask">
-			                       		<div class="info">Quick View</div>
-					                  </div>
-									 <div class="product_container">
-									   <h4>Lorem 2015</h4>
-									   <p>Dresses</p>
-									   <div class="price mount item_price">$99.00</div>
-									   <a class="button item_add cbp-vm-icon cbp-vm-add" href="#">Add to cart</a>
-									 </div>		
-								  </div>
-			                     </div>
-		                      </div>
-							 </a>
-						</li>
-						<li class="last simpleCart_shelfItem">
-							<a class="cbp-vm-image" href="single">
-								<div class="view view-first">
-					   		  <div class="inner_content clearfix">
-								<div class="product_image">
-									<div class="mask1"><img src="images/m3.jpg" alt="image" class="img-responsive zoom-img"></div>
-									<div class="mask">
-			                       		<div class="info">Quick View</div>
-					                  </div>
-									<div class="product_container">
-									   <h4>Lorem 2015</h4>
-									   <p>Dresses</p>
-									    <div class="price mount item_price">$99.00</div>
-									    <a class="button item_add cbp-vm-icon cbp-vm-add" href="#">Add to cart</a>
-									 </div>		
-								  </div>
-			                     </div>
-		                      </div>
-							</a>
-						</li>
-						<li class="simpleCart_shelfItem">
-							<a class="cbp-vm-image" href="single">
-								<div class="view view-first">
-					   		  <div class="inner_content clearfix">
-								<div class="product_image">
-									<div class="mask1"><img src="images/m1.jpg" alt="image" class="img-responsive zoom-img"></div>
-									<div class="mask">
-			                       		<div class="info">Quick View</div>
-					                  </div>
-									<div class="product_container">
-									   <h4>Lorem 2015</h4>
-									   <p>Dresses</p>
-									   <div class="price mount item_price">$99.00</div>
-									    <a class="button item_add cbp-vm-icon cbp-vm-add" href="#">Add to cart</a>
-									 </div>		
-								  </div>
-			                     </div>
-		                      </div>
-							</a>
-						</li>
-						<li class="simpleCart_shelfItem">
-							<a class="cbp-vm-image" href="single">
-								<div class="view view-first">
-					   		  <div class="inner_content clearfix">
-								<div class="product_image">
-									<div class="mask1"><img src="images/m5.jpg" alt="image" class="img-responsive zoom-img"></div>
-									<div class="mask">
-			                       		<div class="info">Quick View</div>
-					                  </div>
-									<div class="product_container">
-									   <h4>Lorem 2015</h4>
-									   <p>Dresses</p>
-									    <div class="price mount item_price">$99.00</div>
-									    <a class="button item_add cbp-vm-icon cbp-vm-add" href="#">Add to cart</a>
-									 </div>		
-								  </div>
-			                     </div>
-		                      </div>
-							</a>
-						</li>
-						<li class="last simpleCart_shelfItem">
-							<a class="cbp-vm-image" href="single">
-								<div class="view view-first">
-					   		  <div class="inner_content clearfix">
-								<div class="product_image">
-									<div class="mask1"><img src="images/m6.jpg" alt="image" class="img-responsive zoom-img"></div>
-									<div class="mask">
-			                       		<div class="info">Quick View</div>
-					                  </div>
-									<div class="product_container">
-									   <h4>Lorem 2015</h4>
-									   <p>Dresses</p>
-									    <div class="price mount item_price">$99.00</div>
-									    <a class="button item_add cbp-vm-icon cbp-vm-add" href="#">Add to cart</a>
-									 </div>		
-								  </div>
-			                     </div>
-		                      </div>
-							</a>
-						</li>
+   	<?php
+   		$connect = connect();
+   		$result = mysqli_query($connect, "SELECT * from sanpham sp,thuonghieu th where phai = 'Nam' and sp.thuonghieu = th.mathuonghieu");
+   		while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){ 
+			$tensp = trim($row['tensp']);		
+			$gia	=trim($row['giatien']);	
+			$donvi=trim($row['donvi']);		
+			$hinhanh=trim($row['hinhanh']);		
+			$thuonghieu = trim($row['tenthuonghieu']);
+	?>
+		<li class="simpleCart_shelfItem">
+			<a class="cbp-vm-image" href="single">
+			  <div class="view view-first">
+	   		  <div class="inner_content clearfix">
+				<div class="product_image">
+					<div class="mask1"><img src="<?php echo $hinhanh ?>" alt="image" class="img-responsive zoom-img"></div>
+					 <div class="mask">
+	               		<div class="info">Quick View</div>
+	                  </div>
+					 <div class="product_container">
+					   <h4><?php echo $tensp ?></h4>
+					   <p><?php echo $thuonghieu ?></p>
+					   <div class="price mount item_price"><?php echo number_format($gia) , " " , $donvi ?></div>
+					   <a class="button item_add cbp-vm-icon cbp-vm-add" href="#">Add to cart</a>
+					 </div>		
+				  </div>
+	             </div>
+	          </div>
+			 </a>
+		</li>	
+	<?php 
+		}
+   		mysqli_close($connect);
+	?>	
 	<!-- more list items -->
 </ul>
                 <script src="js/cbpViewModeSwitch.js" type="text/javascript"></script>
                 <script src="js/classie.js" type="text/javascript"></script>
            
 </div>
+<?php
+	function connect(){	
+		$connect = mysqli_connect("localhost", "root", "") or die(mysql_error());
+		mysqli_set_charset($connect, 'utf8');
+		mysqli_select_db($connect, "watches") or die(mysql_error());
+		return $connect;
+	}
+?>
 @stop
