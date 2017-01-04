f449f848f0436d8b51be4d00c9405eb4bc21ab7d
DoAnWebBanHang <nguyenduy2496@outlook.com.vn>
2017-01-04 22:21:35 +0700

    update resorce

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,73 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
+@extends('layouts.master')
+@section('content')
+   <div class="container-fluid">
+       <div class="row">
+        <div class="col-xs-12">
+            <!-- PAGE CONTENT BEGINS -->
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+            <div class="error-container">
+                <div class="well">
+                    <h1 class="grey lighter smaller">
+                        <span class="blue bigger-125">
+                            <i class="ace-icon fa fa-sitemap"></i>
+                            404
+                        </span>
+                        Mục này không tồn tại
+                    </h1>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+                    <hr />
+                    <h3 class="lighter smaller">Chúng tôi tìm kiếm khắp nơi nhưng chúng tôi không thể tìm thấy nó!</h3>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato', sans-serif;
-            }
+                    <div >
+                        <form class="form-search">
+                            <span class="input-icon align-middle">
+                                <i class="ace-icon fa fa-search"></i>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+                                <input type="text" class="search-query" placeholder="Nhập thông tin cần tìm..." />
+                            </span>
+                            <button class="btn btn-sm" type="button">Tìm kiếm!</button>
+                        </form>
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+                        <div class="space"></div>
+                        <h4 class="smaller">Hãy thử một trong các cách sau: </h4>
 
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
+                        <ul >
+                            <li>
+                                <i class="glyphicon glyphicon-hand-right"></i>
+                                Kiểm tra lại địa chỉ của bạn
+                            </li>
+
+                            <li>
+                                <i class="glyphicon glyphicon-hand-right"></i>
+                                Đọc hưỡng dẫn
+                            </li>
+
+                            <li>
+                                <i class="glyphicon glyphicon-hand-right"></i>
+                                Cho cúng tôi biết
+                            </li>
+                        </ul>
+                    </div>
+
+                    <hr />
+                    <div class="space"></div>
+
+                    <div class="center">
+                        <a href="{!!url('')!!}" class="btn btn-grey">
+                            <i class="ace-icon fa fa-arrow-left"></i>
+                            về trang chủ
+                        </a>
+
+                        <a href="#" class="btn btn-primary">
+                            <i class="ace-icon fa fa-tachometer"></i>
+                            Liên hệ quản trị viên
+                        </a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </body>
-</html>
+
+            <!-- PAGE CONTENT ENDS -->
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+   </div>
+@endsection
