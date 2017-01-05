4e3ecc8519d83ab07d8f56a1df69a3339b4b5cf2
DoAnWebBanHang <trinhthinh279@gmail.com>
2017-01-05 08:25:59 +0700

    Edit

--- resources/views/modules/menu.blade.php
+++ resources/views/modules/menu.blade.php
@@ -33,15 +33,7 @@
               <li>
                <a href="{!!url('tin-tuc')!!}" > Tin Tức - Khuyễn Mãi </a>                    
               </li>                                            
-            </ul>
-            <ul class="nav navbar-nav pull-right">
-                <form method = 'post' action = "timkiem">
-                  <input type="hidden" name="_token" value ="{{csrf_token()}}";> 
-                  <input  placeholder="Tên sản phẩm tìm kiếm ..." type="search" name="tukhoa" id="search">
-                  <input type="submit" value="Tìm kiếm">
-                </form>
-              
-            </ul>
+            </ul>           
              <ul class="nav navbar-nav pull-right">
               {{-- <li><a href="{{ url('/admin/home') }}">Vào trang quản trị</a></li> --}}
               <li class="dropdown">
@@ -101,10 +93,12 @@
             </ul>
           </div><!-- /.navbar-collapse -->
         </div> <!-- /row -->
-        <form role="search" style="width: 30%; padding-top: 20px;">
-                  <div class="form-group">
-                      <input type="text" class="form-control" placeholder="Tìm kiếm ...">
-                  </div>
+        <form  method = 'post' action = "timkiem" role="search" style="width: 30%; padding-top: 20px;">
+          <input type="hidden" name="_token" value ="{{csrf_token()}}";> 
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Tên sản phẩm tìm kiếm ..." name="tukhoa" id="tukhoa">
+            </div>
+          </input>
         </form> 
       </div><!-- /container -->
     </nav>    <!-- /main nav -->
